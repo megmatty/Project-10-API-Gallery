9038549f6de3e590446ac1b4aefafd5444ee99fd
meg matty <meghopweb@me.com>
2016-07-11 19:12:53 -0700

    Initial commit
    initial files

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//AJAX Call - Flickr
+var flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+var flickrOpts = {
+    format: "json"
+};
+
+function displayPhotos(data) {
+    var photoHTML = '<ul>';
+    $.each(data.items, function (i, photo){
+       photoHTML += '<li>';
+       photoHTML += '<a href = "' + photo.link + '" >';
+       photoHTML += '<img src = "' + photo.media.m + '" /> </a></li>';
+    });
+    photoHTML += '</ul>';
+    $('#wrapper').html(photoHTML);
+}
+
+$.getJSON(flickrURL, flickrOpts, displayPhotos);
