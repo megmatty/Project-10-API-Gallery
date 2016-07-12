6803c98d1eee803bb2cfed6c7ef5b663e6d7da2a
meg matty <meghopweb@me.com>
2016-07-11 19:54:11 -0700

    working flickr API code
    flex box added to css li items
    added correct tags to flickrOpts
    added correct target (.wrapper instead of #wrapper)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //AJAX Call - Flickr
 var flickrURL = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 var flickrOpts = {
-    tag: "cat",
+    tags: "cat",
     format: "json"
 };
 
@@ -9,11 +9,11 @@ function displayPhotos(data) {
     var photoHTML = '<ul>';
     $.each(data.items, function (i, photo){
        photoHTML += '<li>';
-       photoHTML += '<a href = "' + photo.link + '" >';
-       photoHTML += '<img src = "' + photo.media.m + '" /> </a></li>';
+       photoHTML += '<a href="' + photo.link + '">';
+       photoHTML += '<img src="' + photo.media.m + '"></a></li>';
     });
     photoHTML += '</ul>';
-    $('#wrapper').html(photoHTML);
+    $('.wrapper').html(photoHTML);
 }
 
 $.getJSON(flickrURL, flickrOpts, displayPhotos);
